7a6a11a1cef98b5280afc3ad3fb4c9ee5e3d516e
AlexanderRichert-NOAA <alexander.richert@noaa.gov>
2025-11-13 20:47:12 +0000

    acorn & wcoss2 site updates

--- configs/sites/tier1/acorn/setup.sh
+++ configs/sites/tier1/acorn/setup.sh
@@ -1,4 +1,6 @@
-module load cray-python/3.11.7
+#module load cray-python/3.11.7
+module load gcc/12.1.0
+module load python/3.8.6
 
 ## Go, Rust repo setup for fetcher scripts
 export GOMODCACHE=${SPACK_STACK_DIR}/cache/go

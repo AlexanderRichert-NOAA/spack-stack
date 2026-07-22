103cf11f1e6c9d6bd7201f195dd36502efb9bbef
Alex Richert <alexander.richert@noaa.gov>
2026-07-22 15:47:39 -0700

    Add nimbus-early-testing site

---
+++ configs/sites/tier2/nimbus-early-testing/setup.sh
@@ -0,0 +1,5 @@
+# Optionally point to a specific Python to use for driving Spack
+
+## Go, Rust repo setup for fetcher scripts
+export GOMODCACHE=${SPACK_STACK_DIR}/cache/go
+export CARGO_HOME=${SPACK_STACK_DIR}/cache/cargo

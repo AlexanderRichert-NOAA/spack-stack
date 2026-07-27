3b950f20799ddde4c71a963f536ec3891c1f271c
Alex Richert <alexander.richert@noaa.gov>
2026-07-27 08:09:49 -0700

    update nimbus-early-testing install script

--- configs/sites/tier2/nimbus-early-testing/install_on_unconfigured_node.sh
+++ configs/sites/tier2/nimbus-early-testing/install_on_unconfigured_node.sh
@@ -1,5 +1,7 @@
 # Download/run this script by itself, as it will download spack-stack and spack-helpers
 
+set -x
+
 tee > /tmp/oneAPI.repo << EOF
 [oneAPI]
 name=Intel® oneAPI repository
@@ -11,19 +13,22 @@ gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.
 EOF
 
 sudo mv /tmp/oneAPI.repo /etc/yum.repos.d/.
-sudo dnf install intel-oneapi-toolkit
-
-dnf install intel-oneapi-compiler-dpcpp-cpp-and-cpp-classic-2023.2.1.x86_64 intel-oneapi-compiler-fortran-2023.2.1.x86_64
-
-dnf install openmpi openmpi-devel
-
-dnf install slurm-devel slurm-slurmd
+sudo dnf install \
+        intel-oneapi-compiler-fortran-2026.1 \
+        intel-oneapi-compiler-dpcpp-cpp-2026.1.0 \
+        intel-oneapi-mpi intel-oneapi-mpi-devel \
+        intel-oneapi-openmp-2026.1 \
+        intel-oneapi-compiler-dpcpp-cpp-and-cpp-classic-2023.2.1.x86_64 \
+        intel-oneapi-compiler-fortran-2023.2.1.x86_64 \
+        openmpi openmpi-devel \
+        slurm-devel slurm-slurmd
 
 git clone --recurse-submodules https://github.com/AlexanderRichert-NOAA/spack-stack -b nimbus-early-testing
 git clone https://github.com/NOAA-EMC/spack-helpers
 
 pushd spack-stack/
 export SPACK_STACK_SITE=nimbus-early-testing
+export SPACK_STACK_TIER=tier2
 . setup.sh
 popd
 . spack-helpers/source_me.sh
